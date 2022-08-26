ec233a5fb5159ed7abd74dcb1e0815adc82631c6
kandoradev <kandoradev@gmail.com>
2022-08-26 16:35:40 -0400

    src-files

---
+++ src/components/TodoModal.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { MdOutlineClose } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { addTodo } from '../slices/todoSlice';
+import styles from '../styles/modules/modal.module.scss';
+import Button from './Button';
+
+function TodoModal({ modalOpen, setModalOpen }) {
+  const [title, setTitle] = useState('');
+  const [status, setStatus] = useState('incomplete');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && status) {
+      dispatch(
+        addTodo({
+          id: uuid(),
+          title,
+          status,
+          time: new Date().toLocaleDateString(),
+        })
+      );
+      toast.success('Task Added Successfully');
+    }
+  };
+
+  return (
+    modalOpen && (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+            tabIndex={0}
+            role="button"
+          >
+            <MdOutlineClose />
+          </div>
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <h1 className={styles.formtitle}> ADD TASK</h1>
+            <label htmlFor="title">
+              Title
+              <input
+                type="text"
+                is="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
+            <label htmlFor="status">
+              Status
+              <select
+                name="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="incomplete">Incomplete</option>
+                <option value="complete">Complete</option>
+              </select>
+            </label>
+            <div className={styles.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Task
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  );
+}
+
+export default TodoModal;
