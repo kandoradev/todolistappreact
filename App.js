2d037ee3bb8c2518f14757455a239596f5331daf
kandoradev <kandoradev@gmail.com>
2022-08-31 16:24:52 -0400

    files-01

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import AppContent from './components/AppContent';
 import AppHeader from './components/AppHeader';
 import PageTitle from './components/PageTitle';
-import style from './styles/modules/app.module.scss';
+import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
-    <div className="container">
-      <PageTitle>TODO LIST</PageTitle>
-      <div className={style.app__wrapper}>
-        <AppHeader />
-        <AppContent />
+    <>
+      <div className="container">
+        <PageTitle>TODO LIST</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
       </div>
-    </div>
+      <Toaster
+        toastOptions={{
+          position: 'bottom-left',
+          style: {
+            fontSize: '1.5rem',
+          },
+        }}
+      />
+    </>
   );
 }
 
